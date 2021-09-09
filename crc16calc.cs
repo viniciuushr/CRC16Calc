8aa0ec23b31f270ae71251eeb5b0ce47c0ca9b62
Vinicius Henrique Ribeiro <40045024+viniciuushr@users.noreply.github.com>
2021-09-09 09:02:05 -0300

    CRC16 CCITT-FALSE
    This code generate a CRC16 CCITT-FALSE

---
+++ crc16calc.cs
@@ -0,0 +1,17 @@
+//this code generate a CRC16 CCITT-FALSE
+public string CalcCRC16(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+            for (int i = 0; i < data.Length; i++)
+            {
+                crc ^= (ushort)(data[i] << 8);
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x8000) > 0)
+                        crc = (ushort)((crc << 1) ^ 0x1021);
+                    else
+                        crc <<= 1;
+                }
+            }
+            return crc.ToString("X4");
+        }
